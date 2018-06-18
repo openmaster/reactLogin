5d8f2c7cce7d714000c9cc0c737690afd075153b
Vishavjeet Singh <vishav4@gmail.com>
2018-06-18 00:47:19 -0400

    disabled button on http request

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -10,30 +10,33 @@ class Login extends React.Component {
 		super(props);
 		this.state = {
 			username: '',
-			password: ''
+			password: '',
+			loading: false
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.loading = false;
 	}
 	
-	
+
 	handleChange(e){
 		this.setState({[e.target.name]: e.target.value});
 	}
-	
 
 	handleSubmit(e){
-
+		this.setState({loading: true})
 		e.preventDefault();
+		var s = this;
 		const body = {username: this.state.username, password: this.state.password};
 		axios.post('https://logintes.herokuapp.com/login', body).then(function(result) {
 		  localStorage.setItem("user", JSON.stringify(result.data));
+		  s.setState({loading: false})
 		  history.push('/dashboard')
 		}).catch(function(err) {
+			s.setState({loading: false})
 			console.log(err);
 		});
 	}
+	
 
 	render() {
 
@@ -49,7 +52,8 @@ class Login extends React.Component {
 			<label>Password</label>
 			<input type='password' name="password" placeholder='Password' value={this.state.password} onChange={this.handleChange} />
 			</Form.Field>
-			<Button  type='submit'>Submit</Button>
+			<Button disabled={this.state.loading} loading={this.state.loading} type='submit'>Submit</Button>
+		
 			</Form>
 			</Responsive>
 			</Segment.Group>
